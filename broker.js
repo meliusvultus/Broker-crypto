3641604bada4534015c1af819c0e706a84e40057
meliusvultus <Irighereaphiensolomons@gmail.com>
2025-10-19 18:23:33 +0000

    second comit

--- broker.js
+++ broker.js
@@ -6,7 +6,7 @@ const port = 3000
 
 
 app.set('view engine', 'ejs')
-app.set('views', '__dirname + views')
+app.set('views', '__dirname + /views')
 app.set('layout', 'layouts/brokertemplate')
 app.use(expressEjsLayouts)
 app.use(express.static('public'))

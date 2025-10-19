01c089c5643b4c33230615e1578897ee6c744801
meliusvultus <Irighereaphiensolomons@gmail.com>
2025-10-19 15:20:17 +0000

    initial comit

---
+++ broker.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import expressEjsLayouts from 'express-ejs-layouts'
+import mainpageRouter from './controller/mainpage.js'
+const app = express()
+const port = 3000
+
+
+app.set('view engine', 'ejs')
+app.set('views', '__dirname + views')
+app.set('layout', 'layouts/brokertemplate')
+app.use(expressEjsLayouts)
+app.use(express.static('public'))
+
+app.listen(port, () => {console.log('up and running')})
+ 

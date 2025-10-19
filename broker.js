5f800489df2dded0b0aabc6c1515bb5c55e78bb2
meliusvultus <Irighereaphiensolomons@gmail.com>
2025-10-19 19:11:57 +0000

    views linked with process.cwd()

--- broker.js
+++ broker.js
@@ -1,15 +1,17 @@
 import express from 'express'
 import expressEjsLayouts from 'express-ejs-layouts'
 import mainpageRouter from './controller/mainpage.js'
+import path from 'path'
 const app = express()
 const port = 3000
 
 
 app.set('view engine', 'ejs')
-app.set('views', '__dirname + /views')
+app.set('views', path.join(process.cwd(), 'views'))
 app.set('layout', 'layouts/brokertemplate')
 app.use(expressEjsLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.use('/', mainpageRouter)
 
 app.listen(port, () => {console.log('up and running')})
--- controller/mainpage.js
+++ controller/mainpage.js
@@ -4,4 +4,4 @@ const router = express.Router();
 export default router;
 
 router.get('/', (req, res) => {
-    res.render('brokertemplate');});
+    res.render('mainpage');});

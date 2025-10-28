c0d72f474d24137db55b756414ee9b6de3838d21
meliusvultus <Irighereaphiensolomons@gmail.com>
2025-10-28 06:45:06 +0000

    new static upgrades

--- broker.js
+++ broker.js
@@ -10,9 +10,9 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(process.cwd(), 'views'))
 app.set('layout', 'layouts/brokertemplate')
 app.use(expressEjsLayouts)
-app.use(express.static('public'))
+app.use(express.static(path.join(process.cwd(), 'public')))
+app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/', mainpageRouter)
 
 app.listen(port, () => {console.log('up and running')})
- 

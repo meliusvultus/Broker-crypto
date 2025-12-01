2fdab73d7d71cbe30ef7f392f5408031a7df4cca
meliusvultus <irighereaphiensolomons@gmail.com>
2025-12-01 01:19:28 +0100

    recent update

--- controller/mainpage.js
+++ controller/mainpage.js
@@ -4,6 +4,14 @@ const router = express.Router();
 export default router;
 
 router.get('/', (req, res) => {
-    res.render('partials/mainpage');});
+    res.render('partials/mainpage');
+});
 router.get('/about', (req, res) => {
+    res.render('partials/about')
+})
+router.get('/login', (req, res) => {
+    res.render('partials/login')
+});
+router.get('/register', (req, res) => {
+    res.render('partials/register')
 })
